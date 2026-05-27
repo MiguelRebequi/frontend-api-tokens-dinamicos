a92a024c3701d5175734ff7ad623d7efd892eac0
MiguelRebequi <130229587+MiguelRebequi@users.noreply.github.com>
2026-05-27 00:22:05 -0300

    fix: ajustado a lógica de segurança de sessão logada na página dos tokens

--- src/pages/tokens/tokens.js
+++ src/pages/tokens/tokens.js
@@ -2,7 +2,6 @@ import { gerenciarNavbarAtiva } from '../../scripts/commons/navbar.js';
 import { formatarDataAtual } from '../../scripts/commons/utils.js';
 import { garantirLogoffESeguranca } from '../../scripts/commons/seguranca.js';
 
-garantirLogoffESeguranca("../../index.html");
 
 // Variáveis de escopo global do arquivo
 let inputCodigo, telaInicial, telaSucesso, telaAlerta;
