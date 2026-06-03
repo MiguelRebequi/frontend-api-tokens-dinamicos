643588dc4092fdf28d59ceca9439436f748acb91
MiguelRebequi <130229587+MiguelRebequi@users.noreply.github.com>
2026-06-03 19:12:27 -0300

    fix: Ajustando o botão de mais opções na página de token

--- src/pages/tokens/tokens.js
+++ src/pages/tokens/tokens.js
@@ -1,4 +1,4 @@
-import { gerenciarNavbarAtiva } from '../../scripts/commons/navbar.js';
+import { gerenciarNavbarAtiva, iniciarDropdownMaisOpcoesMobile } from '../../scripts/commons/navbar.js';
 import { formatarDataAtual } from '../../scripts/commons/utils.js';
 import { garantirLogoffESeguranca } from '../../scripts/commons/seguranca.js';
 
@@ -17,6 +17,9 @@ let mesOffsetSelecionado = -1;
 
 document.addEventListener('DOMContentLoaded', () => {
     gerenciarNavbarAtiva();
+    iniciarDropdownMaisOpcoesMobile();
+    window.addEventListener('resize', iniciarDropdownMaisOpcoesMobile);
+    
     inicializarValidacaoToken();
     inicializarNavegacaoAbas();
     inicializarFiltrosEAvancados();
